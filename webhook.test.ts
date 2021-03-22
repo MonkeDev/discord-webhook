c90c299a197f94528652eff072ac23db9c581684
BurnedBed <53361510+BurnieBed@users.noreply.github.com>
2021-03-21 22:02:30 -0500

    response

--- webhook.test.ts
+++ webhook.test.ts
@@ -1,6 +1,6 @@
 import discordwebhook from "./index.ts";
 
-const webhook = new discordwebhook("webhook_url");
+const webhook = new discordwebhook('https://discord.com/api/webhooks/796141040968794162/XjYIteJO3dpOZnQEUT6nyl_e-teoHQHVp72g-OUdCKSEJ9GnrJ663pTB9_Nz2gJ1ejnd');
 
-await webhook.send("Hello, this is a test.")
-  .then(console.log);
+webhook.send('Hello, this is a test.')
+    .then(console.log);
