531946943e7e8ce5ec3ef7b12350dbc03202b9af
Swaggger <71473617+BurnedBed@users.noreply.github.com>
2021-03-22 15:29:48 -0500

    Delete webhook.test.ts

--- webhook.test.ts
+++
@@ -1,6 +0,0 @@
-import discordwebhook from "./index.ts";
-
-const webhook = new discordwebhook('https://discord.com/api/webhooks/796141040968794162/XjYIteJO3dpOZnQEUT6nyl_e-teoHQHVp72g-OUdCKSEJ9GnrJ663pTB9_Nz2gJ1ejnd');
-
-webhook.send('Hello, this is a test.')
-    .then(console.log);
