2cc4bf28a8d7c51630c5eb82ca54e44a2ca5b1ac
BurnedBed <53361510+BurnieBed@users.noreply.github.com>
2021-03-22 11:51:05 -0500

    I guess mafia dont want

--- index.ts
+++ index.ts
@@ -1,52 +1,11 @@
 const webhookurlRegex = /https:\/\/(www\.|)discord\.com\/api\/webhooks\//g
-interface credentials {
-  webhook_url?: string;
-  botToken?: string;
-}
-interface availParams {
-  name: string,
-  avatar?: any
-}
 export default class discordwebhook {
   url: string;
-  botToken: string;
-  constructor(credentials?: credentials) {
-    this.url = credentials?.webhook_url ?? "https://discord.com/api/webhooks/";
-    this.botToken = credentials?.botToken ?? '';
+  constructor(webhookUrl: string) {
+    this.url = webhookUrl ?? "https://discord.com/api/webhooks/";
     if (this.url.search(webhookurlRegex) !== 0) {
       throw new Error("Please provide a webhook URL, this.url");
     }
-  }
-  
-  get(webhookId: string) {
-    return fetch(`https://discord.com/api/v8/webhooks/${encodeURIComponent(webhookId)}`, {
-      method: "GET",
-      headers: { "Authorization": `Bot ${this.botToken}`, "content-type": "application/json" },
-    });
-  }
-  getChannelWebhooks(channelId: string) {
-    return fetch(`https://discord.com/api/v8/channels/${encodeURIComponent(channelId)}/webhooks`, {
-      method: "GET",
-      headers: { "Authorization": `Bot ${this.botToken}`, "content-type": "application/json" },
-    });
-  }
-
-  getGuildWebhooks(guildId: string) {
-    return fetch(`https://discord.com/api/v8/guilds/${encodeURIComponent(guildId)}/webhooks`, {
-      method: "GET",
-      headers: { "Authorization": `Bot ${this.botToken}`, "content-type": "application/json" },
-    });
-  }
-  create(channelId: string, params: availParams) {
-    let data: availParams = {
-      name: params.name
-    }
-    if(params.avatar) data['avatar'] = params.avatar;
-    return fetch(`https://discord.com/api/v8/channels/${encodeURIComponent(channelId)}/webhooks`, {
-      method: "POST",
-      headers: { "Authorization": `Bot ${this.botToken}`, "content-type": "application/json" },
-      body: JSON.stringify(data),
-    });
   }
 
   execute(message: string, params?: object) {
